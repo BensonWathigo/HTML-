ef01295b822c40bd96fe2485147679b161349070
BensonWathigo <bensonwathigo21@gmail.com>
2024-04-30 17:17:19 +0300

    first commit

--- weather_app/script.js
+++ weather_app/script.js
@@ -1,10 +1,10 @@
 const apiKey= "4e88fde4600122cf52bfb8d5db19f41a";
-const apiUrl = "https://api.openweathermap.org/data/3.5/weather";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-const locationInput = document.getElementById('locationInput');
-const searchButton = document.getElementById('searchButton');
+const locationInput = document.getElementById("locationInput");
+const searchButton = document.getElementById("searchButton");
 const locationElement =document.getElementById('location');
-const temperatureElement = document.getElementById('temperature');
+const temperatureElement = document.getElementById("temperature");
 const descrptionElement = document.getElementById("description");
 
 searchButton.addEventListener("click", () => {
@@ -17,20 +17,20 @@ searchButton.addEventListener("click", () => {
 });
 
 function fetchWeather(location) {
-    const url = `${apiUrl} ? = ${location} &api id = ${apiKey} and units metrics`
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`
 
     fetch(url)
-    .then((response) => response.json)
+    .then((response) => response.json())
     .then((data) => {
         locationElement.textContent = data.name;
-        temperatureElement.textContent = `${Math.round(data.main.temp)} c`;
+        temperatureElement.textContent =`${Math.round(data.main.temp)} ºC`;
         descrptionElement.textContent = data.weather[0].description;
         //countryElement.textContent = data.sys.country;
 
     })
 
     .catch((error) => {
-        console.log('Error fetching weather data.', error);
+        console.error('Error fetching weather data.', error);
     });
 
 }
